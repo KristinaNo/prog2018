fff21a5e554d467f66d24f15bcac5e0f4dd92e08
Kristina Sadykova <kristina1997g@mail.ru>
2018-04-07 09:38:56 +0500

    Laba4

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class HashTable
+
+    {
+        class Pair
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+
+        List<List<Pair>> list;
+        /// <summary>
+
+        /// Конструктор контейнера
+
+        /// summary>
+
+        /// size">Размер хэ-таблицы
+
+        public HashTable(int size)
+
+        {
+            list = new List<List<Pair>>();
+            for (int i=0; i<size; i++)
+            {
+                list.Add(new List<Pair>());
+            }
+
+
+        }
+
+        /// 
+
+        /// Метод складывающий пару ключь-значение в таблицу
+
+        /// 
+
+        /// key">
+
+        /// value">
+
+        public void PutPair(object key, object value)
+
+        {
+            var bucketNumber = Math.Abs(key.GetHashCode()) % list.Count; ;
+            foreach(var element in list[bucketNumber])
+            {
+                if (Equals(element.Key, key))
+                {
+                    element.Value = value;
+                    return;
+                }
+            }
+            list[bucketNumber].Add(new Pair { Key = key, Value = value });
+        }
+
+
+
+        /// <summary>
+
+        /// Поиск значения по ключу
+
+        /// summary>
+
+        /// key">Ключь
+
+        /// <returns>Значение, null если ключ отсутствуетreturns>
+
+        public object GetValueByKey(object key)
+
+        {
+            var bucketNumber = Math.Abs(key.GetHashCode()) % list.Count; ;
+            foreach (var element in list[bucketNumber])
+            {
+                if (Equals(element.Key, key))
+                {
+                    return element.Value;
+                }
+            }
+
+            return null;
+        }
+        static void Main()
+        {
+        }
+    }
+}
---
+++ ConsoleApp3/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddThreeElements()
+        {
+            var hashTab = new HashTable(3);
+            hashTab.PutPair(1, "one");
+            hashTab.PutPair(2, "two");
+            hashTab.PutPair(3, "three");
+            Assert.AreEqual(hashTab.GetValueByKey(1), "one");
+            Assert.AreEqual(hashTab.GetValueByKey(2), "two");
+            Assert.AreEqual(hashTab.GetValueByKey(3), "three");
+        }
+
+        [TestMethod]
+        public void TestAddTwoElements()
+        {
+            var hashTab = new HashTable(2);
+            hashTab.PutPair(1, "one");
+            hashTab.PutPair(1, "two");
+            Assert.AreEqual(hashTab.GetValueByKey(1), "two");
+        }
+
+        [TestMethod]
+        public void TestManyElements()
+        {
+            var hashTab = new HashTable(100000);
+            for (int i=0; i<100000; i++)
+            hashTab.PutPair(i, i+2);
+            Assert.AreEqual(hashTab.GetValueByKey(7905), 7907);
+        }
+
+        [TestMethod]
+        public void TestOtherManyElements()
+        {
+            var hashTab = new HashTable(100000);
+            for (int i = 0; i < 100000; i++)
+                hashTab.PutPair(i, i + 2);
+            for (int i = 0; i < 100000; i++)
+            Assert.AreEqual(hashTab.GetValueByKey(i), i+2);
+        }
+    }
+
+
+    
+        
+    
+}
